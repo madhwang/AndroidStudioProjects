98d9104671436e4251170f1c66a9b40280591889
madhwang <nobelhwang@gmail.com>
2014-11-16 18:13:40 +0900

    step07

--- Step07GridViewPager/app/src/main/java/com/hbi/step07gridviewpager/WearActivity.java
+++ Step07GridViewPager/app/src/main/java/com/hbi/step07gridviewpager/WearActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.wearable.view.CardFragment;
+import android.support.wearable.view.DismissOverlayView;
 import android.support.wearable.view.FragmentGridPagerAdapter;
 import android.support.wearable.view.GridViewPager;
-import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.ImageReference;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 
 /*
@@ -27,11 +31,21 @@ public class WearActivity extends Activity {
 	GridViewPager mPager;
 	MyGridAdapter mAdapter;
 
+	GestureDetectorCompat gDector;
+
+	DismissOverlayView dismissView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+
+/*
+		// activity_wear 를 사용해서 화면을 구성하는 경우
 		setContentView(R.layout.activity_wear);
 		final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+		gDector = new GestureDetectorCompat(this,new LongPressListener());
+
 		stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
 			@Override
 			public void onLayoutInflated(WatchViewStub stub) {
@@ -44,9 +58,30 @@ public class WearActivity extends Activity {
 
 				//페이저와 어댑터 연결
 				mPager.setAdapter(mAdapter);
+
+				dismissView = (DismissOverlayView)findViewById(R.id.overlay);
+
 			}
 		});
 
+*/
+
+		/*
+		rect_activity_wear 를 사용해서 화면을 구성하는 경우
+		 */
+		setContentView(R.layout.rect_activity_wear);
+		gDector = new GestureDetectorCompat(this,new LongPressListener());
+
+		//필요한 객체의 참조값
+		mPager = (GridViewPager) findViewById(R.id.fragment_container);
+
+		//생성자의 인자로 FragmentManager 객체를 전달하면서 객체 생성하기
+		mAdapter = new MyGridAdapter(getFragmentManager());
+
+		//페이저와 어댑터 연결
+		mPager.setAdapter(mAdapter);
+
+		dismissView = (DismissOverlayView)findViewById(R.id.overlay);
 	}
 
 	//커스텀 어댑터 객체를 만들 클래스 정의하기
@@ -56,6 +91,21 @@ public class WearActivity extends Activity {
 		//생성자.
 		public MyGridAdapter(FragmentManager fm) {
 			super(fm);
+		}
+
+		@Override
+		public ImageReference getBackground(int row, int column) {
+			//return super.getBackground(row, column);
+			switch (row) {
+				case 0:
+					return ImageReference.forDrawable(R.drawable.image1);
+				case 1:
+					return ImageReference.forDrawable(R.drawable.image2);
+				case 2:
+					return ImageReference.forDrawable(R.drawable.image3);
+				default:
+					return ImageReference.forDrawable(R.drawable.image1);
+			}
 		}
 
 		//파라메터 값에 해당하는 행,열의 값을 Fragment 로 만들어 리턴한다.
@@ -87,7 +137,7 @@ public class WearActivity extends Activity {
 			//Bundle 의 사용은 CardFragment 와 약속된 내용.
 			Bundle args = new Bundle();
 			args.putString(CardFragment.KEY_TITLE,"행 : " + row ); //CardFragment 의 제목
-			args.putString(CardFragment.KEY_TEXT," 열: " + col); //CardFragment 의 내용
+			args.putString(CardFragment.KEY_TEXT, " 열: " + col); //CardFragment 의 내용
 
 			//Fragment 객체를 생성한다.
 			ContentFragment frag = new ContentFragment();
@@ -97,6 +147,23 @@ public class WearActivity extends Activity {
 
 			//생성한 Fragment 객체를 리턴해준다.
 			return frag;
+		}
+	}
+
+
+	//터치 이벤트를 잡는다.
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		return gDector.onTouchEvent(ev) || super.dispatchTouchEvent(ev);
+	}
+
+
+
+	//길게 눌렀을 때 실행되는 클래스
+	class LongPressListener extends GestureDetector.SimpleOnGestureListener{
+		@Override
+		public void onLongPress(MotionEvent e) {
+			dismissView.show();
 		}
 	}
 }
