f1c39c11891964a1a9cc91f3fe7aaaf297445cde
madhwang <nobelhwang@gmail.com>
2014-11-16 17:09:32 +0900

    step07

--- Step07GridViewPager/app/src/main/java/com/hbi/step07gridviewpager/WearActivity.java
+++ Step07GridViewPager/app/src/main/java/com/hbi/step07gridviewpager/WearActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.wearable.view.CardFragment;
 import android.support.wearable.view.FragmentGridPagerAdapter;
 import android.support.wearable.view.GridViewPager;
 import android.support.wearable.view.WatchViewStub;
@@ -29,6 +30,7 @@ public class WearActivity extends Activity {
 				mPager = (GridViewPager) findViewById(R.id.fragment_container);
 			}
 		});
+
 	}
 
 	//커스텀 어댑터 객체를 만들 클래스 정의하기
@@ -43,7 +45,8 @@ public class WearActivity extends Activity {
 		//파라메터 값에 해당하는 행,열의 값을 Fragment 로 만들어 리턴한다.
 		@Override
 		public Fragment getFragment(int row, int col) {
-			return null;
+			//해당 행과 열에 해당하는 Fragment 객체를 리턴해준다.
+			return ContentFragment.newInstance(row,col);
 		}
 
 
@@ -58,6 +61,28 @@ public class WearActivity extends Activity {
 		@Override
 		public int getColumnCount(int i) {
 			return cols;
+		}
+	}
+
+	//CardFragment 클래스를 상속받은 클래스를 정의한다.
+	 public static class ContentFragment extends CardFragment{
+		//행과 열을 전달 받아서 해당 Fragment 객체를 만들어서 리턴해 주는 메서드
+		public static ContentFragment newInstance(int row, int col)
+		{
+			//Fragment에 전달한 데이터를 Bundle 객체에 담는다.
+			//Bundle 의 사용은 CardFragment 와 약속된 내용.
+			Bundle args = new Bundle();
+			args.putString(CardFragment.KEY_TITLE,"행 : " + row ); //CardFragment 의 제목
+			args.putString(CardFragment.KEY_TEXT," 열: " + col); //CardFragment 의 내용
+
+			//Fragment 객체를 생성한다.
+			ContentFragment frag = new ContentFragment();
+
+			//인자를 전달한다.
+			frag.setArguments(args);
+
+			//생성한 Fragment 객체를 리턴해준다.
+			return frag;
 		}
 	}
 }
