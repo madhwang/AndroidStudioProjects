7231231cf9453214ba41b19f88159678d2b29028
madhwang <nobelhwang@gmail.com>
2014-11-16 15:26:30 +0900

    Step05

---
+++ Step05Notification/app/src/main/java/com/hbi/step05notification/MyConstant.java
@@ -0,0 +1,9 @@
+package com.hbi.step05notification;
+
+/**
+ * final 상수값만 관리할 클래스 정의하기
+ */
+public class MyConstant {
+	public static final int NOTI_NEW_MESSAGE= 1;
+	public static final int NOTI_DETAIL_VIEW=999;
+}
--- Step05Notification/app/src/main/java/com/hbi/step05notification/WearActivity.java
+++ Step05Notification/app/src/main/java/com/hbi/step05notification/WearActivity.java
@@ -2,6 +2,8 @@ package com.hbi.step05notification;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -33,14 +35,22 @@ public class WearActivity extends Activity {
 	//알람 띄우기 버튼을 눌렀을 때
 	public void showNotification(View v)
 	{
+		//인텐트 전달자 객체 얻어 내기
+		PendingIntent p = PendingIntent.getActivity(this,
+				MyConstant.NOTI_DETAIL_VIEW,
+				new Intent(this,TargetActivity.class), //TargetActivity 클래스를 실행할 인텐트
+				PendingIntent.FLAG_UPDATE_CURRENT);
+
+		//알람 객체 만들기
 		Notification noti = new NotificationCompat.Builder(this)
 				.setContentTitle("알림제목")
-				.setContentText("내용...")
+				.setContentText("내용내용내용내용내용내용내용내용내용내용내용")
 				.setSmallIcon(R.drawable.forest)
+				.addAction(R.drawable.forest,"자세히보기",p)
 				.setAutoCancel(true)
 				.build();
 
-		NotificationManagerCompat.from(this).notify(1,noti);
+		NotificationManagerCompat.from(this).notify(MyConstant.NOTI_NEW_MESSAGE,noti);
 
 		//종료
 		finish();
