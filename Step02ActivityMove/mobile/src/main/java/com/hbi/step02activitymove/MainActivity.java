d1f76c850ca17436ad6fcbab365fa62b19ef4367
madhwang <nobelhwang@gmail.com>
2014-11-16 10:10:28 +0900

    commit

---
+++ Step02ActivityMove/mobile/src/main/java/com/hbi/step02activitymove/MainActivity.java
@@ -0,0 +1,51 @@
+package com.hbi.step02activitymove;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    //버튼을 눌렀을때 호출되는 메소드
+    public void move(View v)
+    {
+        //액티비티를 이동하겠다는   Intent 객체 생성하기
+        Intent intent = new Intent(this,SubActivity.class);
+        //Intent 객체를 인자로 전달하면서 액티비티 이동하는 메소드호출
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Step02ActivityMove/wear/src/main/java/com/hbi/step02activitymove/WearActivity.java
@@ -0,0 +1,33 @@
+package com.hbi.step02activitymove;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.TextView;
+
+public class WearActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wear);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+
+    //버튼을 눌렀을때 호출되는 메소드
+    public void move(View v)
+    {
+        Intent intent=new Intent(this, WearSubActivity.class);
+        startActivity(intent);
+    }
+}
