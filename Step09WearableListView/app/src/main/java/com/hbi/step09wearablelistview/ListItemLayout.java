e83e47fdd6d72eaa85380b41cfc1c0a164bd72b5
madhwang <madhwang@gmail.com>
2014-11-22 18:13:46 +0900

    step09

--- Step09WearableListView/app/src/main/java/com/hbi/step09wearablelistview/ListItemLayout.java
+++ Step09WearableListView/app/src/main/java/com/hbi/step09wearablelistview/ListItemLayout.java
@@ -9,13 +9,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
- * Created by user on 2014-11-22.
+ * Created by lee on 2014-11-22.
  */
-public class ListItemLayout  extends LinearLayout implements WearableListView.Item{
+public class ListItemLayout extends LinearLayout
+	implements WearableListView.Item{
 
-	//현재 배율을 저장할 멤버 필드
-	private float mScale;
-
+	//현재 배율을 저장할 맴버필드
+	float mScale;
 	float mFadedTextAlpha;
 	int mFadedCircleColor;
 	int mChosenCircleColor;
@@ -23,83 +23,61 @@ public class ListItemLayout  extends LinearLayout implements WearableListView.It
 	TextView mName;
 
 	public ListItemLayout(Context context) {
-		super(context);
+		super(context, null);
 	}
 
 	public ListItemLayout(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		super(context, attrs, 0);
 	}
 
-	public ListItemLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		mFadedTextAlpha =  getResources().getInteger(R.integer.faded_alpha)/100f;
-		mFadedCircleColor = getResources().getColor(R.color.wl_gray);
-		mChosenCircleColor = getResources().getColor(R.color.wl_blue);
+	public ListItemLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
 	}
-
-
-	/**
-	 * 레이아웃(각각의 아이템) 전개가 완료 되었을 때 호출되는 메서드
-	 */
+	//레이아웃(각각의 아이템) 전개가 완료 되었을때 호출되는 메소드
 	@Override
 	protected void onFinishInflate() {
 		super.onFinishInflate();
+
+		mFadedTextAlpha = getResources().getInteger(R.integer.faded_alpha)/100f;
+		mFadedCircleColor = getResources().getColor(R.color.wl_gray);
+		mChosenCircleColor = getResources().getColor(R.color.wl_blue);
+
+
 		mCircle = (ImageView) findViewById(R.id.circle);
 		mName = (TextView) findViewById(R.id.time_text);
 	}
-
-	/**
-	 * 최소 크기 배율
-	 * @return
-	 */
+	//최소 크기 배율
 	@Override
 	public float getProximityMinValue() {
 		return 1f; //원본크기
 	}
-
-	/**
-	 * 최대 크기 배율
-	 * @return
-	 */
+	//최대 크기 배율
 	@Override
 	public float getProximityMaxValue() {
-		return 1.5f;
+		return 1.5f; //원본 크기의 1.5배
 	}
-
-	/**
-	 * 현재 스케일 값을 리턴해주는 메서드
-	 * @return
-	 */
+	//현재 스케일 값을 리턴해주는 메소드
 	@Override
 	public float getCurrentProximityValue() {
-		return 0;
+		return mScale;
 	}
-
-	/**
-	 * 현재 스케일 value 값이 인자로 전달되는 메소드
-	 * @param scale
-	 */
+	//현재 스케일 value 값이 인자로 전달되는 메소드
 	@Override
 	public void setScalingAnimatorValue(float scale) {
-		//인자로 전달되는 현재 배율값을 멤버 필드에 저장한다.
-		this.mScale = scale;
-		this.mCircle.setScaleX(scale);
-		this.mCircle.setScaleY(scale);
+		//인자로 전달되는 현재 배율값을 맴버필드에 저장한다.
+		mScale=scale;
+		mCircle.setScaleX(scale);
+		mCircle.setScaleY(scale);
 	}
-
-	/**
-	 * 확대가 시작될때 호출되는 메서드
-	 */
+	//확대가 시작되었을때 호출되는 메소드
 	@Override
 	public void onScaleUpStart() {
 		mName.setAlpha(1f);
 		((GradientDrawable) mCircle.getDrawable())
 			.setColor(mChosenCircleColor);
 	}
-
-	/**
-	 * 축소가 시작될때 호출되는 메서드
-	 */
+	//축소가 시작되었을때 호출되는 메소드
 	@Override
 	public void onScaleDownStart() {
 		((GradientDrawable) mCircle.getDrawable())
@@ -107,5 +85,3 @@ public class ListItemLayout  extends LinearLayout implements WearableListView.It
 		mName.setAlpha(mFadedTextAlpha);
 	}
 }
-
-
--- Step09WearableListView/app/src/main/java/com/hbi/step09wearablelistview/WearActivity.java
+++ Step09WearableListView/app/src/main/java/com/hbi/step09wearablelistview/WearActivity.java
@@ -1,53 +1,95 @@
 package com.hbi.step09wearablelistview;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WearableListView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Model(data)는 Adapter 에게 데이터를 전달하고
- * Adapter는 Mode(data) 정보를 이용해서 셀을 만들어서 WearableListView 에게 전달한다.
- */
 public class WearActivity extends Activity {
-
+	public static final int MODEL_COUNTS = 10;
+	public static final String TAG = "WearActivity";
 	WearableListView listView;
+	//리스트뷰 셀에 출력할 모델 객체를 담은 배열객체
+	private ListViewItem[] models = new ListViewItem[MODEL_COUNTS];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wear);
-
-		//WearableListView의 참조값 얻어와서 멤버 필드에 저장하기.
+		init();
+	}
+	//초기화 메소드
+	public void init(){
+		//셈플 모델객체 넣어두기
+		models[0]=new ListViewItem("김구라1", 1000);
+		models[1]=new ListViewItem("김구라2", 2000);
+		models[2]=new ListViewItem("김구라3", 3000);
+		models[3]=new ListViewItem("김구라4", 4000);
+		models[4]=new ListViewItem("김구라5", 5000);
+		models[5]=new ListViewItem("김구라6", 6000);
+		models[6]=new ListViewItem("김구라7", 7000);
+		models[7]=new ListViewItem("김구라8", 8000);
+		models[8]=new ListViewItem("김구라9", 9000);
+		models[9]=new ListViewItem("김구라10", 10000);
 		listView = (WearableListView)findViewById(R.id.list_view);
+		listView.setAdapter(new ListViewAdapter(this));
 	}
 
-	/**
-	 * 초기화 메서드
-	 */
-	public void init()
-	{
-
-	}
-
-	/**
-	 * 어댑터 객체를 생성할 클래스 정의하기
-	 */
-	class ListViewAdapter extends WearableListView.Adapter{
-
-		@Override
-		public WearableListView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-			return null;
+	//셀에 출력할 모델객체를 얻어낼 클래스
+	static class ListViewItem{
+		long duration;
+		String label;
+		public ListViewItem(String label, long duration) {
+			this.label = label;
+			this.duration = duration;
 		}
 
 		@Override
+		public String toString() {
+			return label;
+		}
+	}
+
+	//ListViewAdapter 객체를 생성할 클래스
+
+	final class ListViewAdapter extends WearableListView.Adapter{
+		Context context;
+		LayoutInflater inflater;
+
+		//생성자
+		public ListViewAdapter(Context context){
+			this.context=context;
+			inflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public WearableListView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+			//레이아웃전개후
+			View v = inflater.inflate(R.layout.list_item, null);
+			//뷰홀더객체를 만들고
+			WearableListView.ViewHolder holder = new WearableListView.ViewHolder(v);
+			//홀더 객체를 리턴해준다.
+			return holder;
+		}
+
+		//해당 뷰홀더 객체에 출력할 내용을 바인딩해준다.
+		@Override
 		public void onBindViewHolder(WearableListView.ViewHolder viewHolder, int i) {
+			ImageView img = (ImageView)viewHolder.itemView.findViewById(R.id.circle); //
+			TextView view = (TextView)viewHolder.itemView.findViewById(R.id.time_text);
+			view.setText(models[i].label);
+			viewHolder.itemView.setTag(i);
 
 		}
 
 		@Override
 		public int getItemCount() {
-			return 0;
+			return MODEL_COUNTS;
 		}
 	}
 }
