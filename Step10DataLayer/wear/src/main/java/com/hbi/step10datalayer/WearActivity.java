9ac927e2edc972242e880c74959c72bd6a1dc2b1
madhwang <madhwang@gmail.com>
2014-11-29 14:35:17 +0900

    step10
    step10

--- Step10DataLayer/wear/src/main/java/com/hbi/step10datalayer/WearActivity.java
+++ Step10DataLayer/wear/src/main/java/com/hbi/step10datalayer/WearActivity.java
@@ -2,9 +2,11 @@ package com.hbi.step10datalayer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -16,11 +18,9 @@ public class WearActivity extends Activity
 	implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,MessageApi.MessageListener
 {
 
-	private TextView mTextView;
-
 	//GoogleApiClient 객체
 	GoogleApiClient gClient;
-
+	EditText console;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,7 +29,8 @@ public class WearActivity extends Activity
 		stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
 			@Override
 			public void onLayoutInflated(WatchViewStub stub) {
-				mTextView = (TextView) stub.findViewById(R.id.text);
+				//EditText 값 받아오기
+				console = (EditText)findViewById(R.id.console);
 			}
 		});
 
@@ -49,8 +50,11 @@ public class WearActivity extends Activity
 
 	@Override
 	protected void onStop() {
-		super.onStop();
+		//액티비티가 비활성화 될때 마무리 작업을 한다.
 		gClient.disconnect();
+		Wearable.MessageApi.removeListener(gClient,this);
+
+		super.onStop();
 	}
 
 	/**
@@ -59,18 +63,19 @@ public class WearActivity extends Activity
 	 */
 	@Override
 	public void onConnected(Bundle bundle) {
+		printLog("onConnected()");
 		//메시지 도착 리스너를 등록한다.
 		Wearable.MessageApi.addListener(gClient,this);
 	}
 
 	@Override
 	public void onConnectionSuspended(int i) {
-
+		printLog("onConnectionSuspended()");
 	}
 
 	@Override
 	public void onConnectionFailed(ConnectionResult connectionResult) {
-
+		printLog("onConnectionFailed()");
 	}
 
 	/**
@@ -81,5 +86,29 @@ public class WearActivity extends Activity
 		//전달된 Path(메시지,문자열)을 읽어온다.
 		String msg = messageEvent.getPath();
 		Log.e("WearActivity","전달된 문자열 :"+msg);
+
+		//EditText에 출력하기
+		printLog(msg);
 	}
+
+	//로그를 찍는 메소드
+	public void printLog(String msg){
+		//출력할 문자열을 메세지 객체에 담는다.
+		Message m=new Message();
+		m.obj = msg;
+		//핸들러에 메세지를 보낸다.
+		handler.sendMessage(m);
+	}
+
+	//핸들러 객체 정의하기
+	Handler handler=new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			//전달된 문자열을 얻어온다.
+			String str = (String)msg.obj;
+			//EditText 에 개행기호와 함께 출력하기
+			console.append(str+"\n");
+		}
+	};
+
 }
