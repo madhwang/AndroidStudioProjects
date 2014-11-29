f654a36ddc39787a4abfe1ad70aefb8cc15a4249
madhwang <madhwang@gmail.com>
2014-11-29 12:38:43 +0900

    step10
    step10

--- Step10DataLayer/mobile/src/main/java/com/hbi/step10datalayer/MainActivity.java
+++ Step10DataLayer/mobile/src/main/java/com/hbi/step10datalayer/MainActivity.java
@@ -1,43 +1,52 @@
 package com.hbi.step10datalayer;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.EditText;
-import android.os.Handler;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
+import java.util.Collection;
+import java.util.HashSet;
 
-public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
 
-	EditText console;
-
-	//GoogleApiClient 객체를 멤버필드에 담기 위해
+public class MainActivity extends Activity
+	implements GoogleApiClient.ConnectionCallbacks,
+	GoogleApiClient.OnConnectionFailedListener{
+	//필요한 맴버필드 정의하기
+	EditText console, inputText;
+	//GooglaApiClient 객체를 맴버필드에 담기위해
 	GoogleApiClient gClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//EditText의 참조값을 얻어와서 멤버 필드에 저장하기
-		console = (EditText)findViewById(R.id.inputText);
-
-		//GoogleApiClient 객체를 생성해서 멤버필드에 담기
-		gClient = new GoogleApiClient.Builder(this)
-							.addApi(Wearable.API)
-							.addConnectionCallbacks(this)
-							.addOnConnectionFailedListener(this)
-							.build();
+		//EditText 의 참조값을 얻어와서 맴버필드에 저장하기
+		console = (EditText)findViewById(R.id.console);
+		inputText = (EditText)findViewById(R.id.inputText);
+		//GoogleApiClient 객체를 생성해서 맴버필드에 담기
+		gClient=new GoogleApiClient.Builder(this)
+			.addApi(Wearable.API)
+			.addConnectionCallbacks(this)
+			.addOnConnectionFailedListener(this)
+			.build();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		//GoogleApiClient에 연결 요청을 한다.
+		//GoogleApiClient 에 연결 요청을 한다.
 		gClient.connect();
 	}
 
@@ -48,65 +57,99 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 		super.onStop();
 	}
 
-	/**
-	 * 전송 버튼을 눌렀을때 호출되는 메서드
-	 * @param v
-	 */
-	public void send(View v)
-	{
+	//전송 버튼을 눌렀을때 호출되는 메소드
+	public void send(View v){
+		//입력한 문자열을 읽어온다.
+		String msg = inputText.getText().toString();
+		if(msg.equals(""))return;
+		//비동기 작업에 전달할 파라미터를 배열에 담는다.
+		String[] params=new String[1];
+		params[0]=msg;
+		//파라미터를 전달하면서 비동기 작업 실행 시키기
+		new SendMessageTask().execute(params);
 	}
-
-
-	public void printLog(String msg)
-	{
-		//출력할 문자열을 메시지 객체에 담는다
-		Message m = new Message();
+	//로그를 찍는 메소드
+	public void printLog(String msg){
+		//출력할 문자열을 메세지 객체에 담는다.
+		Message m=new Message();
 		m.obj = msg;
-
-		//핸들러에 메시지를 보낸다
+		//핸들러에 메세지를 보낸다.
 		handler.sendMessage(m);
-
 	}
 
-	/**
-	 * 핸들러 객체 정의
-	 */
-	Handler handler = new Handler(){
+	//핸들러 객체 정의하기
+	Handler handler=new Handler(){
 		@Override
-		public void handleMessage(Message msg)
-		{
-			// 전달된 문자열을 읽어온다.
-			 String str = (String)msg.obj;
+		public void handleMessage(Message msg) {
+			//전달된 문자열을 얻어온다.
+			String str = (String)msg.obj;
 			//EditText 에 개행기호와 함께 출력하기
-			console.append(str+'\n');
+			console.append(str+"\n");
 		}
 	};
-
-
-	/**
-	 * GoogleApiClient에 연결이 되었을때 호출되는 메서드
-	 * @param bundle
-	 */
+	//GoogleApiClient 에 연결이 되었을때 호출되는 메소드
 	@Override
 	public void onConnected(Bundle bundle) {
 		printLog("onConnected()");
 	}
-
-	/**
-	 * GoogleApiClient에 연결이 연기 혹은 지연되었을때 호출되는 메서드
-	 * @param i
-	 */
+	//GoogleApiClient 에 연결이 연기 혹은 지연되었을때 호출되는 메소드
 	@Override
 	public void onConnectionSuspended(int i) {
 		printLog("onConnectionSuspended()");
 	}
-
-	/**
-	 * GoogleApiClient에 연결이 실패 되었을때 호출되는 메서드
-	 * @param connectionResult
-	 */
+	//GoogleApiClient 에 연결이 실패되었을때 호출되는 메소드
 	@Override
 	public void onConnectionFailed(ConnectionResult connectionResult) {
 		printLog("onConnectionFailed()");
+	}
+	//비동기 작업 객체를 생성할 클래스
+	class SendMessageTask extends AsyncTask<String,Void,Void>{
+		//UI 스레드가 아닌 다른 스래드에서 작업되는 메소드
+		@Override
+		protected Void doInBackground(String... params) {
+			//전송할 문자열을 읽어온다.
+			String msg = params[0];
+			//연결된 node id 를 얻어온다.
+			Collection<String> nodes=getNodes();
+			//반복문 돌면서 모든 node 에 전송한다.
+			for(final String node:nodes){
+				//콘솔에 node id 를 출력해보기
+				printLog(node);
+				//MessageApi 를 이용해서 전송한다.
+				//(GoogleApiClient, node id, msg , byte[] )
+				Wearable.MessageApi.sendMessage(
+					gClient,
+					node,
+					msg,
+					new byte[0]
+				).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+					@Override
+					public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+						//전송 성공이라면
+						if(sendMessageResult.getStatus().isSuccess()){
+							printLog("전송 성공!");
+						}else{//전송 실패라면
+							printLog("전송 실패!");
+						}
+					}
+				});
+			}
+			return null;
+		}
+	}
+
+	//연결된 Node List 를 리턴하는 메소드
+	public Collection<String> getNodes(){
+		HashSet<String> results = new HashSet<String>();
+		//연결된 Node List 를 읽어온다.
+		NodeApi.GetConnectedNodesResult nodes=
+			Wearable.NodeApi.getConnectedNodes(gClient)
+				.await();
+		//반복문돌면서 연결된 node 의 id 를 HashSet 에 담는다.
+		for(Node node : nodes.getNodes()){
+			results.add(node.getId());
+		}
+		//연결된 node 의 아이디 값을 담고 있는 HashSet 객체 리턴해주기
+		return results;
 	}
 }
--- Step10DataLayer/wear/src/main/java/com/hbi/step10datalayer/WearActivity.java
+++ Step10DataLayer/wear/src/main/java/com/hbi/step10datalayer/WearActivity.java
@@ -3,11 +3,23 @@ package com.hbi.step10datalayer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
-public class WearActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
+public class WearActivity extends Activity
+	implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,MessageApi.MessageListener
+{
 
 	private TextView mTextView;
+
+	//GoogleApiClient 객체
+	GoogleApiClient gClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,54 @@ public class WearActivity extends Activity {
 				mTextView = (TextView) stub.findViewById(R.id.text);
 			}
 		});
+
+		gClient = new GoogleApiClient.Builder(this)
+							.addApi(Wearable.API)
+							.addConnectionCallbacks(this)
+							.addOnConnectionFailedListener(this)
+							.build();
+	}
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		gClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		gClient.disconnect();
+	}
+
+	/**
+	 * GoogleApiClient에 연결 되었을때 호출되는 메서드
+	 * @param bundle
+	 */
+	@Override
+	public void onConnected(Bundle bundle) {
+		//메시지 도착 리스너를 등록한다.
+		Wearable.MessageApi.addListener(gClient,this);
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+
+	}
+
+	/**
+	 * Message가 도착했을때 호출되는 메서드
+	 */
+	@Override
+	public void onMessageReceived(MessageEvent messageEvent) {
+		//전달된 Path(메시지,문자열)을 읽어온다.
+		String msg = messageEvent.getPath();
+		Log.e("WearActivity","전달된 문자열 :"+msg);
 	}
 }
